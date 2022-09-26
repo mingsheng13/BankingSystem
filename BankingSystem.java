f637140cf71fbfd6aeaf9c8aee3e79b18f50602e
chai <ming.chai@ucdconnect.ie>
2022-09-26 13:46:49 +0000

    created methods

---
+++ BankingSystem.java
@@ -0,0 +1,35 @@
+public class BankingSystem {
+    public static void main(String[] args) {
+        System.out.println("first");
+    }
+
+    public void withdrawal()
+    {
+
+    }
+
+    public void deposit()
+    {
+
+    }
+
+    public void checkBalance()
+    {
+
+    }
+
+    public void accountOpen()
+    {
+
+    }
+
+    public void accountDelete()
+    {
+
+    }
+
+    public void accountUpdate()
+    {
+
+    }
+}
