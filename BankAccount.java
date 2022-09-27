b180c4f46ced1cf0e1380e4e069f6fa389610d1b
Naux334 <xinyuan.he334@gmail.com>
2022-09-27 16:43:35 +0100

    give balance system in

--- BankAccount.java
+++ BankAccount.java
@@ -3,15 +3,17 @@ import java.util.LinkedList;
 class accountDetails
 {
     int id;
+
+    int balance;
     String lastName;
     String firstName;
     String phone_number;
     String email;
     String age;
     String date_of_create;
-    public accountDetails(int id, String lastName, String firstName, String phone_number, String email, String age, String date_of_create)
+    public accountDetails(int id, int balance, String lastName, String firstName, String phone_number, String email, String age, String date_of_create)
     {
-        this.id = id; this.lastName = lastName; this.firstName = firstName; this.phone_number = phone_number; this.email = email; this.age = age; this.date_of_create = date_of_create;
+        this.id = id; this.balance = balance; this.lastName = lastName; this.firstName = firstName; this.phone_number = phone_number; this.email = email; this.age = age; this.date_of_create = date_of_create;
     }
     
     
@@ -20,9 +22,9 @@ public class BankAccount
 {
     LinkedList<accountDetails> user = new LinkedList<accountDetails>();;
 
-    public void accountOpen(int id, String lastName, String firstName, String phone_number, String email, String age, String date_of_create)
+    public void accountOpen(int id, int balance, String lastName, String firstName, String phone_number, String email, String age, String date_of_create)
     {
-        accountDetails tmp = new accountDetails(id, lastName, firstName, phone_number, email, age, date_of_create);
+        accountDetails tmp = new accountDetails(id, balance, lastName, firstName, phone_number, email, age, date_of_create);
         user.add(tmp);
         System.out.println(user.getFirst().toString());
     }
--- BankingSystem.java
+++ BankingSystem.java
@@ -3,7 +3,7 @@ public class BankingSystem {
     public static void main(String[] args)
     {
         BankAccount first = new BankAccount();
-        first.accountOpen(1, "aa", "aaa", "aaaa", "aaaa", "11", "123");
+        first.accountOpen(1, 0,"aa", "aaa", "aaaa", "aaaa", "11", "123");
     }
 
     public void withdrawal()
